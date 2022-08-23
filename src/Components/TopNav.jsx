5230dd4349d4cfcbe094dcdfe412bacffb97925a
karangorania <karangoraniya@gmail.com>
2022-08-23 10:57:00 +0530

    initial commit 🚀

---
+++ src/Components/TopNav.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import UAuth from '@uauth/js';
+
+export const TopNav = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    // uauth();
+    return () => {};
+  }, [isLogin]);
+
+  const uauth = new UAuth({
+    clientID: 'ba816047-d507-4f7d-a8ac-e801f309bc21',
+    redirectUri: 'http://localhost:3000',
+    scope: 'openid wallet',
+  });
+
+  const loginHandler = async () => {
+    try {
+      const authorization = await uauth.loginWithPopup();
+
+      console.log(authorization);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logoutHandler = async () => {
+    await uauth.logout();
+    console.log('Logged out with Unstoppable');
+    setIsLogin(false);
+  };
+
+  return (
+    <>
+      <nav
+        style={{ background: '#000' }}
+        className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full"
+      >
+        <div className="mb-2 sm:mb-0">
+          <a
+            href="/home"
+            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+          >
+            <img src="logo.png" width={'100'} />
+          </a>
+        </div>
+        <div style={{ margin: 'auto 0' }}>
+          <button
+            type="button"
+            className="nft-gradient bg-blue-400 text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white"
+            onClick={loginHandler}
+          >
+            Login with Unstoppable
+          </button>
+          {/* <button
+            type="button"
+            className="nft-gradient bg-blue-400 text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white"
+            onClick={logoutHandler}
+          >
+            Logout
+          </button> */}
+        </div>
+      </nav>
+    </>
+  );
+};
